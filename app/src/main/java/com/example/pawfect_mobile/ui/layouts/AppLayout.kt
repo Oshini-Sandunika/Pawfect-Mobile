4af76bc50fe9131a54bc7c5572b11337d7fa6b5e
yehan2002 <yehanjaya2002@gmail.com>
2026-07-11 23:38:30 +0530

    Add bg image.

--- app/src/main/java/com/example/pawfect_mobile/ui/layouts/AppLayout.kt
+++ app/src/main/java/com/example/pawfect_mobile/ui/layouts/AppLayout.kt
@@ -1,5 +1,6 @@
 package com.example.pawfect_mobile.ui.layouts
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.consumeWindowInsets
@@ -11,6 +12,11 @@ import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.ImageShader
+import androidx.compose.ui.graphics.ShaderBrush
+import androidx.compose.ui.graphics.TileMode
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.pawfect_mobile.R
 import com.example.pawfect_mobile.ui.theme.AppTheme
@@ -22,6 +28,17 @@ fun AppLayout(
     content: @Composable (BoxScope.() -> Unit),
 ) {
     val snackState = remember { SnackbarHostState() }
+    val image = ImageBitmap.imageResource(R.drawable.background_tile)
+    val brush =
+        remember(image) {
+            ShaderBrush(ImageShader(image, TileMode.Repeated, TileMode.Repeated))
+        }
+    Box(
+        Modifier
+            .fillMaxSize()
+            .background(brush)
+    ) {
+    }
 
     AppTheme {
         Scaffold(
@@ -35,6 +52,8 @@ fun AppLayout(
                     } else {
                         Modifier.padding(it)
                     }
+                        .background(brush, alpha = 0.15f)
+                        .fillMaxSize()
                 )
             }
         )
