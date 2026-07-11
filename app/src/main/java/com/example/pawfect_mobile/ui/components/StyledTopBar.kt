32beb45a50e907b7cef7ca1ccc92bacbc76df5a1
yehan2002 <yehanjaya2002@gmail.com>
2026-07-12 04:15:55 +0530

    Add icon to header.

--- app/src/main/java/com/example/pawfect_mobile/ui/components/StyledTopBar.kt
+++ app/src/main/java/com/example/pawfect_mobile/ui/components/StyledTopBar.kt
@@ -1,5 +1,10 @@
 package com.example.pawfect_mobile.ui.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -8,19 +13,37 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.pawfect_mobile.R
 
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
 fun StyledTopBar(title: String, goBack: (() -> Unit)? = null) {
     TopAppBar(
-        title = { Text(title) },
+        title = {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(title)
+                Image(
+                    painter = painterResource(id = R.drawable.pawfect_logo),
+                    contentDescription = "",
+                    modifier = Modifier.size(48.dp)
+                )
+            }
+        },
         colors = androidx.compose.material3.TopAppBarDefaults.topAppBarColors(
             containerColor = Color.White.copy(
                 alpha = 0.75f
             )
         ),
-        
+
         navigationIcon = if (goBack != null) {
             {
                 IconButton(onClick = goBack) {
